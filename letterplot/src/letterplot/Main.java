009e07fc75a928b0ce9eb11292a43252e451f0c8
Ilya Mizus <bigfatbrowncat@gmail.com>
2015-02-09 18:57:53 +0300

    Added cyrillic chars to the default 6x8 font

--- letterplot/src/letterplot/Main.java
+++ letterplot/src/letterplot/Main.java
@@ -3,6 +3,7 @@ package letterplot;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.EnumSet;
 
 import nostalgia.Core;
@@ -54,11 +55,66 @@ public class Main {
 	private static Color backColor = new Color(0.2f, 0.2f, 0.2f); 
 	private static Color foreHighlightColor = new Color(0.5f, 0.85f, 0.6f); 
 	private static Color backHighlightColor = new Color(0.25f, 0.5f, 0.3f); 
+	private static Color screenBackgroundColor = new Color(0.3f, 0.3f, 0.35f);
+	private static Color darkDialogBackColor = new Color(0.2f, 0.2f, 0.25f, 0.85f); 
 	
 	//private static Font font;
 	private static Font editingFont, systemFont;
 	
 	private static String pangram = "Sphinx of black quartz, judge my vow.";
+	private static String addSymbolHexCodeInput = "";
+	
+	private static boolean isHexDigit(char c) {
+		return (c >= '0' && c <= '9') || 
+	    (c >= 'a' && c <= 'f') || 
+	    (c >= 'A' && c <= 'F');
+	}
+	
+	private static String[] easySplit(String s, char sep) {
+		StringBuilder sb = new StringBuilder();
+		ArrayList<String> parts = new ArrayList<String>();
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) != sep) {
+				sb.append(s.charAt(i));
+			} else {
+				parts.add(sb.toString());
+				sb.setLength(0);
+			}
+		}
+		parts.add(sb.toString());
+		return parts.toArray(new String[] { });
+	}
+	
+	private static boolean canTypeSymbolHexCodeInput(char c) {
+		String[] splitted = easySplit(addSymbolHexCodeInput, '-');
+		if (isHexDigit(c)) {
+			if (splitted.length == 1 && splitted[0].length() < 4 ||
+			    splitted.length == 2 && splitted[1].length() < 4) {
+				
+				return true;
+			} else {
+				return false;
+			}
+		} else if (c == '-') {
+			return splitted.length < 2;
+		} else {
+			return false;
+		}
+	}
+	
+	private static String formatHexCodeOrRange(String input, int digs) {
+		String[] splitted = easySplit(input, '-');
+		for (int i = 0; i < splitted.length; i++) {
+			while (splitted[i].length() < digs) {
+				splitted[i] = "0" + splitted[i];
+			}
+		}
+		if (splitted.length == 1) {
+			return splitted[0].toUpperCase();
+		} else {
+			return (splitted[0] + "-" + splitted[1]).toUpperCase();
+		}
+	}
 	
 	private static void drawPreview(Color fore, Color back, Bitmap bmp, boolean[] letter, int x0, int y0) {
 		for (int i = 0; i < editingFont.getWidth(); i++) {
@@ -90,71 +146,86 @@ public class Main {
 
 	private static int fontWidth = 6, fontHeight = 8;
 
-	private static void loadFont(String filename) {
+	private static boolean loadFont(String filename) {
 		FileInputStream fis = null;
 		try {
 			try {
 				fis = new FileInputStream(filename);
 				editingFont = Font.fromStream(fis);
+				return true;
 			} finally {
 				if (fis != null) fis.close();
 			}
 		} catch (IOException ex) {
 			System.err.println("Can't load the font file");
 			ex.printStackTrace();
-			System.err.println("Creating the new font");
-			editingFont = new Font(fontWidth, fontHeight);
+		}
+		return false;
+	}
+	
+	private static void createFont() {
+		editingFont = new Font(fontWidth, fontHeight);
+		editingFont.addSymbol('A');
+		currentChar = 'A';
+	}
+	
+	private enum State {
+		Global, AddSymbolsDialog
+	}
+
+	private static void drawHotkeys(Bitmap screen, String[] hotKeyDesc) {
+		String[] hotKeys = new String[] { "Esc", " F1", " F2", " F3", " F4", " F5", " F6", " F7", " F8", " F9", "F10", "F11", "F12" };
+		drawHotkeys(screen, hotKeys, hotKeyDesc);
+	}
+	
+	private static void drawHotkeys(Bitmap screen, String[] hotKeys, String[] hotKeyDesc) {
+
+		Painter p = new Painter(screen);
+		p.setForeground(null);
+		p.setBackground(backHighlightColor);
+		p.setFont(systemFont);
+		
+		int hotKeyLenMax = 0;
+		for (int i = 0; i < hotKeys.length; i++) {
+			if (hotKeys[i].length() > hotKeyLenMax) {
+				hotKeyLenMax = hotKeys[i].length(); 
+			}
+		}
+		hotKeyLenMax++;
+		
+		int hotkeysNumberLimit = hotKeyDesc.length + 1;
+		int hotKeySpace, hotKeyDescLenMax;
+		do {
+			hotkeysNumberLimit --;
+			hotKeyDescLenMax = 0;
+			for (int i = 0; i < hotKeyDesc.length; i++) {
+				if (hotKeyDesc[i].length() > hotKeyDescLenMax) {
+					hotKeyDescLenMax = hotKeyDesc[i].length(); 
+				}
+			}
+			hotKeySpace = screen.getWidth() / hotkeysNumberLimit;
+		} while (hotKeySpace < (hotKeyDescLenMax + hotKeyLenMax) * systemFont.getWidth());
+		
+		
+		int hotKeyY = screen.getHeight() - systemFont.getHeight() - 1;
+		for (int i = 0; i < hotkeysNumberLimit; i++) {
+			p.setForeground(null);
+			int x = i * hotKeySpace;
+			p.setBackground(backHighlightColor);
+			p.drawRectangle(x + hotKeyLenMax * systemFont.getWidth(), hotKeyY - 1, x + hotKeySpace, screen.getHeight());
+			
+			p.setForeground(foreHighlightColor);
+			p.drawString(x + hotKeyLenMax * systemFont.getWidth() + 1, hotKeyY, hotKeyDesc[i]);
+			p.setBackground(backColor);
+			p.setForeground(foreColor);
+			p.drawString(x + systemFont.getWidth(), hotKeyY, hotKeys[i]);
 		}
 	}
 	
 	public static void main(String[] args) throws IOException {
 		systemFont = Font.fromStream(Font.class.getResource("font6x8.dat").openStream());
-		//new Font(fontWidth, fontHeight);
-		/*for (char c = '0'; c <= '9'; c++) {
-			font.addSymbol(c);
-		}
-		for (char c = 'A'; c <= 'Z'; c++) {
-			font.addSymbol(c);
-		}
-		for (char c = 'a'; c <= 'z'; c++) {
-			font.addSymbol(c);
-		}
-		font.addSymbol('!');
-		font.addSymbol('?');
-		font.addSymbol('@');
-		font.addSymbol('#');
-		font.addSymbol('$');
-		font.addSymbol('%');
-		font.addSymbol('^');
-		font.addSymbol('&');
-		font.addSymbol('*');
-		font.addSymbol('(');
-		font.addSymbol(')');
-		font.addSymbol('[');
-		font.addSymbol(']');
-		font.addSymbol('{');
-		font.addSymbol('}');
-		font.addSymbol('_');
-		font.addSymbol('-');
-		font.addSymbol('+');
-		font.addSymbol('=');
-		font.addSymbol('/');
-		font.addSymbol('\\');
-		font.addSymbol('|');
-		font.addSymbol('<');
-		font.addSymbol('>');
-		font.addSymbol('\'');
-		font.addSymbol('`');
-		font.addSymbol('~');
-		font.addSymbol('"');
-		font.addSymbol('.');
-		font.addSymbol(',');
-		font.addSymbol(':');
-		font.addSymbol(';');*/
 		
-		editingFont = new Font(fontWidth, fontHeight);
-		editingFont.addSymbol('A');
-		currentChar = 'A';
+		createFont();
 		
 		if (Core.open("LetterPlot", 800, 600, 2)) {
 			Core.setCursorVisibility(false);
@@ -162,7 +233,8 @@ public class Main {
 				
 				private int mouseX = -10, mouseY = -10;
 				private boolean textCurVisible = false;
-				
+				private State state = State.Global;
+
 				boolean isInCells(int x, int y) {
 					return x > cellsX0 && y > cellsY0 &&
 					       x < cellsX0 + cellSize * editingFont.getWidth() &&
@@ -181,7 +253,7 @@ public class Main {
 						boolean[] currentLetter = editingFont.getSymbol(currentChar);
 						Bitmap screen = getScreen();
 						Painter p = new Painter(screen);
-						p.setBackground(new Color(0.3f, 0.3f, 0.35f));
+						p.setBackground(screenBackgroundColor);
 						p.drawRectangle(0, 0, screen.getWidth() - 1, screen.getHeight() - 1);
 						
 						p.setForeground(new Color(0.5f, 0.5f, 0.5f));
@@ -232,48 +304,67 @@ public class Main {
 						int pangramX0 = (int)(screen.getWidth() / 2 - panW / 2);
 						int pangramY0 = (int)(cellsY0 + cellSize * editingFont.getHeight() + paddingBottom / 3);
 						p.drawString(pangramX0, pangramY0, pangram);
+
 						int textCurX = pangramX0 + editingFont.getWidth() * pangram.length();
-						
-						if (textCurVisible) { 
+						if (textCurVisible && state == State.Global) { 
 							p.setForeground(null);
 							p.setBackground(foreColor);
 							p.drawRectangle(textCurX, pangramY0, textCurX + editingFont.getWidth(), pangramY0 + editingFont.getHeight());
 						}
 						
-						// Drawing hot keys
-						//p.setForeground(foreHighlightColor);
-						p.setForeground(null);
-						p.setBackground(backHighlightColor);
-						p.setFont(systemFont);
-						String[] hotKeys = new String[] { "Esc", " F1", " F2", " F3", " F4", " F5", " F6", " F7", " F8", " F9", "F10", "F11", "F12" };
-						String[] hotKeyDesc = new String[] { "Exit", "", "Save", "Load", "", "", "", "", "", "", "", "", "" };
-						
-						int hotkeysNumberLimit = hotKeys.length + 1;
-						int hotKeyWidth, hotKeyDescLenMax;
-						do {
-							hotkeysNumberLimit --;
-							hotKeyDescLenMax = 0;
-							for (int i = 0; i < hotKeyDesc.length; i++) {
-								if (hotKeyDesc[i].length() > hotKeyDescLenMax) {
-									hotKeyDescLenMax = hotKeyDesc[i].length(); 
-								}
-							}
-							hotKeyWidth = screen.getWidth() / hotkeysNumberLimit;
-						} while (hotKeyWidth < (hotKeyDescLenMax + 4) * systemFont.getWidth());
-						
-						
-						int hotKeyY = screen.getHeight() - systemFont.getHeight() - 1;
-						for (int i = 0; i < hotkeysNumberLimit; i++) {
-							p.setForeground(null);
-							int x = i * hotKeyWidth;
-							p.setBackground(backHighlightColor);
-							p.drawRectangle(x + 4 * systemFont.getWidth(), hotKeyY - 1, x + hotKeyWidth, screen.getHeight());
+						// Drawing dialogs over the global screen
+						if (state == State.AddSymbolsDialog) {
+							Painter p2 = new Painter(screen);
+							p2.setBackground(darkDialogBackColor);
+							p2.setForeground(null);
+							p2.drawRectangle(0, 0, screen.getWidth(), screen.getHeight());
 							
-							p.setForeground(foreHighlightColor);
-							p.drawString(x + 4 * systemFont.getWidth() + 1, hotKeyY, hotKeyDesc[i]);
-							p.setBackground(backColor);
-							p.setForeground(foreColor);
-							p.drawString(x + systemFont.getWidth(), hotKeyY, hotKeys[i]);
+							String addSymbolsTitle = "Add symbols to the font";
+							String[] addSymbolsDescription = new String[] {
+									"Type here the unicode character index",
+									"as 4 digits in hexadecimal format (like 01AB or FFFF).",
+									"You can also type '-' after some digits, to create",
+									"a range of codes (like 0410-042F)"
+							};
+
+							p2.setFont(systemFont);
+							p2.setForeground(foreHighlightColor);
+							int y0 = screen.getHeight() / 2 - 7 * p2.getFont().getHeight();
+							int x0 = screen.getWidth() / 2 - p2.stringWidth(addSymbolsTitle) / 2;
+							p2.drawString(x0, y0, addSymbolsTitle);
+
+							p2.setForeground(foreColor);
+							y0 += p2.getFont().getHeight();
+
+							for (int i = 0; i < addSymbolsDescription.length; i++) {
+								x0 = screen.getWidth() / 2 - p2.stringWidth(addSymbolsDescription[i]) / 2;
+								y0 += p2.getFont().getHeight();
+								p2.drawString(x0, y0, addSymbolsDescription[i]);
+							}
+							
+							String formattedRange = formatHexCodeOrRange(addSymbolHexCodeInput, 4);
+							x0 = screen.getWidth() / 2 - p2.stringWidth(formattedRange) / 2;
+							y0 += p2.getFont().getHeight() * 3;
+							p2.drawString(x0, y0, formattedRange);
+							
+							int addDigstextCurX = x0 + editingFont.getWidth() * formattedRange.length();
+							if (textCurVisible) { 
+								p.setForeground(null);
+								p.setBackground(foreColor);
+								p.drawRectangle(addDigstextCurX - editingFont.getWidth(), y0, addDigstextCurX, y0 + editingFont.getHeight());
+							}
+
+						}
+						
+						// Drawing hot keys
+						if (state == State.Global) {
+							String[] hotKeyDesc = new String[] { "Exit", "New", "Save", "Load", "AddS", "RemS" };
+							drawHotkeys(screen, hotKeyDesc);
+						} else {
+							String[] hotKeys = new String[] { "  Esc", "Enter" };
+							String[] hotKeyDesc = new String[] { "Cancel", "OK" };
+							
+							drawHotkeys(screen, hotKeys, hotKeyDesc);
 						}
 						
 						// Drawing mouse cursor at last
@@ -334,37 +425,94 @@ public class Main {
 				
 				@Override
 				public void key(Key key, int scancode, KeyState state, Modifiers modifiers) {
-					if (key == Key.ESCAPE && state == KeyState.PRESS && modifiers.isEmpty()) {
-						Core.close();
-					} else if (key == Key.RIGHT && (state == KeyState.PRESS || state == KeyState.REPEAT) && modifiers.isEmpty()) {
-						Character nextChar = editingFont.nextSymbol(currentChar);
-						if (nextChar != null) {
-							currentChar = nextChar;
-						} else {
-							currentChar = editingFont.getFirstSymbol();
+					if (this.state == State.Global) {
+						if (key == Key.ESCAPE && state == KeyState.PRESS && modifiers.isEmpty()) {
+							Core.close();
+						} else if (key == Key.RIGHT && (state == KeyState.PRESS || state == KeyState.REPEAT)) {
+							if (modifiers.isEmpty()) {
+								Character nextChar = editingFont.nextSymbol(currentChar);
+								if (nextChar != null) {
+									currentChar = nextChar;
+								} else {
+									currentChar = editingFont.getFirstSymbol();
+								}
+							} else if (modifiers.equals(Modifiers.of(Modifier.ALT))) {
+								for (int k = 0; k < 10; k++) {
+									Character nextChar = editingFont.nextSymbol(currentChar);
+									if (nextChar != null) 
+										currentChar = nextChar;
+									else
+										break;
+								}
+							}
+						} else if (key == Key.LEFT && (state == KeyState.PRESS || state == KeyState.REPEAT)) {
+							if (modifiers.isEmpty()) {
+								Character nextChar = editingFont.previousSymbol(currentChar);
+								if (nextChar != null) {
+									currentChar = nextChar;
+								} else {
+									currentChar = editingFont.getLastSymbol();
+								}
+							} else if (modifiers.equals(Modifiers.of(Modifier.ALT))) {
+								for (int k = 0; k < 10; k++) {
+									Character nextChar = editingFont.previousSymbol(currentChar);
+									if (nextChar != null) 
+										currentChar = nextChar;
+									else
+										break;
+								}
+							}
+						} else if (key == Key.F1 && state == KeyState.PRESS && modifiers.isEmpty()) {
+							createFont();
+						} else if (key == Key.F2 && state == KeyState.PRESS && modifiers.isEmpty()) {
+							saveFont("font.dat");
+						} else if (key == Key.F3 && state == KeyState.PRESS && modifiers.isEmpty()) {
+							loadFont("font.dat");
+						} else if (key == Key.F4 && state == KeyState.PRESS && modifiers.isEmpty()) {
+							this.state = State.AddSymbolsDialog;
+						} else if (key == Key.BACKSPACE && (state == KeyState.PRESS || state == KeyState.REPEAT) && modifiers.isEmpty()) {
+							if (!pangram.equals("")) {
+								pangram = pangram.substring(0, pangram.length() - 1);
+							}
 						}
-					} else if (key == Key.LEFT && (state == KeyState.PRESS || state == KeyState.REPEAT) && modifiers.isEmpty()) {
-						Character nextChar = editingFont.previousSymbol(currentChar);
-						if (nextChar != null) {
-							currentChar = nextChar;
-						} else {
-							currentChar = editingFont.getLastSymbol();
-						}
-					} else if (key == Key.F2 && state == KeyState.PRESS && modifiers.isEmpty()) {
-						saveFont("font.dat");
-					} else if (key == Key.F3 && state == KeyState.PRESS && modifiers.isEmpty()) {
-						loadFont("font.dat");
-					} else if (key == Key.BACKSPACE && (state == KeyState.PRESS || state == KeyState.REPEAT) && modifiers.isEmpty()) {
-						if (!pangram.equals("")) {
-							pangram = pangram.substring(0, pangram.length() - 1);
+					} else if (this.state == State.AddSymbolsDialog) {
+						if (key == Key.ESCAPE && state == KeyState.PRESS && modifiers.isEmpty()) {
+							this.state = State.Global;
+						} else if (key == Key.ENTER && state == KeyState.PRESS && modifiers.isEmpty()) {
+							this.state = State.Global;
+							
+							if (!addSymbolHexCodeInput.contains("-")) {
+								int addCode = Integer.parseInt(addSymbolHexCodeInput, 16);
+								editingFont.addSymbol((char)addCode);
+								currentChar = (char)addCode;
+							} else {
+								String[] spl = easySplit(addSymbolHexCodeInput, '-');
+								int rangeStart = Integer.parseInt(spl[0], 16);
+								int rangeEnd = Integer.parseInt(spl[1], 16);
+								for (int c = rangeStart; c <= rangeEnd; c++) {
+									editingFont.addSymbol((char)c);
+								}
+								currentChar = (char)rangeStart;
+							}
+							
+						} else if (key == Key.BACKSPACE && (state == KeyState.PRESS || state == KeyState.REPEAT) && modifiers.isEmpty()) {
+							if (!addSymbolHexCodeInput.equals("")) {
+								addSymbolHexCodeInput = addSymbolHexCodeInput.substring(0, addSymbolHexCodeInput.length() - 1);
+							}
 						}
 					}
 				}
 				
 				@Override
 				public void character(char character, Modifiers modifiers) {
-					if (modifiers.isEmpty() || modifiers.equals(Modifiers.of(Modifier.SHIFT))) {
-						pangram += character;
+					if (this.state == State.Global) {
+						if (modifiers.isEmpty() || modifiers.equals(Modifiers.of(Modifier.SHIFT))) {
+							pangram += character;
+						}
+					} else if (this.state == State.AddSymbolsDialog) {
+						if (canTypeSymbolHexCodeInput(character)) {
+							addSymbolHexCodeInput += character;
+						}
 					}
 				}
 			});
--- nostalgia/src/java/nostalgia/graphics/Font.java
+++ nostalgia/src/java/nostalgia/graphics/Font.java
@@ -3,16 +3,18 @@ package nostalgia.graphics;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
 import java.util.BitSet;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 
 public class Font {
 	private int width, height;
-	private LinkedHashMap<Character, boolean[]> letters = new LinkedHashMap<Character, boolean[]>();
+	private HashMap<Character, boolean[]> letters = new HashMap<Character, boolean[]>();
 	
 	public Character nextSymbol(char character) {
 		int cur = -1;
 		Character[] chars = letters.keySet().toArray(new Character[] { });
+		Arrays.sort(chars);
 		for (int i = 0; i < chars.length; i++) {
 			if (chars[i].equals(character)) cur = i;
 		}
@@ -28,6 +30,7 @@ public class Font {
 	public Character previousSymbol(char character) {
 		int cur = -1;
 		Character[] chars = letters.keySet().toArray(new Character[] { });
+		Arrays.sort(chars);
 		for (int i = 0; i < chars.length; i++) {
 			if (chars[i].equals(character)) cur = i;
 		}
@@ -41,10 +44,22 @@ public class Font {
 	}
 
 	public Character getFirstSymbol() {
-		return (Character) letters.keySet().toArray()[0];
+		Character[] chars = letters.keySet().toArray(new Character[] { });
+		Arrays.sort(chars);
+		if (chars.length > 0) {
+			return chars[0];
+		} else {
+			return null;
+		}
 	}
 	public Character getLastSymbol() {
-		return (Character) letters.keySet().toArray()[letters.keySet().size() - 1];
+		Character[] chars = letters.keySet().toArray(new Character[] { });
+		Arrays.sort(chars);
+		if (chars.length > 0) {
+			return chars[chars.length - 1];
+		} else {
+			return null;
+		}
 	}
 	
 	public Font(int width, int height) {
@@ -73,13 +88,42 @@ public class Font {
 		return height;
 	}
 	
+	private static void writeInt(OutputStream out, int val) throws IOException {
+		int b1 = val & 0xff;
+		int b2 = (val >> 8) & 0xff;
+		int b3 = (val >> 16) & 0xff;
+		int b4 = val >> 24;
+		out.write(b1);
+		out.write(b2);
+		out.write(b3);
+		out.write(b4);
+	}
+	
+	private static int readInt(InputStream in) throws IOException {
+		int b1 = in.read();
+		int b2 = in.read();
+		int b3 = in.read();
+		int b4 = in.read();
+		int res = b4;
+		res <<= 8;
+		res += b3;
+		res <<= 8;
+		res += b2;
+		res <<= 8;
+		res += b1;
+		return res;
+	}
+	
 	public void toStream(OutputStream out) throws IOException {
-		out.write(width); out.write(height);
+		writeInt(out, width); writeInt(out, height);
+		//out.write(width); out.write(height);
 		
 		Character[] keys = letters.keySet().toArray(new Character[] {});
-		out.write(keys.length);
+		writeInt(out, keys.length);
+		//out.write(keys.length);
 		for (int k = 0; k < keys.length; k++) {
-			out.write(keys[k]);
+			writeInt(out, (int)(keys[k]));
+			//out.write((int)(keys[k]));
 			
 			BitSet bs = new BitSet(width * height);
 			boolean[] letter = letters.get(keys[k]);
@@ -87,19 +131,20 @@ public class Font {
 				bs.set(i, letter[i]);
 			}
 			byte[] letterBytes = bs.toByteArray();
-			out.write(letterBytes.length);
+			writeInt(out, letterBytes.length);
+			//out.write(letterBytes.length);
 			out.write(letterBytes);
 		}
 	}
 	
 	public static Font fromStream(InputStream in) throws IOException {
-		int width = in.read();
-		int height = in.read();
+		int width = readInt(in);
+		int height = readInt(in);
 		Font res = new Font(width, height);
-		int charsCount = in.read();
+		int charsCount = readInt(in);
 		for (int k = 0; k < charsCount; k++) {
-			char key = (char) in.read();
-			int letterBytesLength = in.read();
+			char key = (char) readInt(in);
+			int letterBytesLength = readInt(in);
 			byte[] letterBytes = new byte[letterBytesLength];
 			in.read(letterBytes);
 			BitSet bs = BitSet.valueOf(letterBytes);
